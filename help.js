b53bdc4adb11a5047caff70e74f2b5f2be534d82
Dinataa <103869704+asbiiiiiii@users.noreply.github.com>
2022-05-03 18:09:03 +0700

    Create help.js

---
+++ help.js
@@ -0,0 +1,70 @@
+*Dinata Bot MD*
+
+Simple Menu Bot By Reza Dinata
+▹ Name: ${pushname}
+▹ Role: ${role}
+
+Catatan: *Abaikan <> pada command*
+${readmore}
+*INFO MENU*
+${no}menu
+${no}speed
+${no}runtime
+${no}q <reply>
+
+*SENDER MENU*
+${no}setmenu <teks/restart>
+${no}reaction <emoji>
+${no}join <link gc>
+
+*TEXT MAKER*
+${no}textprolist
+${no}textpro <textpro|teks>
+
+*STALKER*
+${no}ffstalk <id>
+${no}mlstalk <id|region>
+
+*SEARCH*
+${no}groupwa <teks>
+${no}textprosearch <title>
+${no}identifymusic <reply audio>
+${no}ytsearch <query>
+
+*GAME*
+${no}bahasapanda
+${no}outbp
+${no}tsbp <tabel|teks>
+
+*DOWNLOADER*
+${no}tiktok <Url>
+${no}tiktokmp3 <Url>
+${no}mediafire <Url>
+${no}alquranmp3 <Url>
+${no}pinterest <title>
+
+*OTHER MENU*
+${no}obfus <code>
+${no}emailto <gmail|teks>
+${no}translate <teks|kode negara>
+
+*GROUP MENU*
+${no}antilink <on/off>
+${no}welcome <on/off>
+${no}left <on/off>
+${no}setwelcome <teks>
+${no}setleft <teks>
+${no}setsubject <teks>
+${no}setdesc <teks>
+${no}leave
+${no}linkgc
+
+*OWNER MENU*
+▹ [Eval] *>* <code>
+▹ [Eval 2] *=>* <code>
+▹ [Exec] *$* <code>
+${no}mode <self/public>
+${no}autoread <on/off>
+${no}autoreact <on/off>
+${no}setreact <emote>
+${no}boardcast <teks>
